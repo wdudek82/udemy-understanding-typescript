91c46b08e03cf820b9cbc75376e55ade979d8df4
Wojtek Dudek <wdudek82@gmail.com>
2018-07-29 18:56:10 +0200

    Added function interface

--- ts/interfaces.js
+++ ts/interfaces.js
@@ -8,8 +8,33 @@ function changeName(person, newName) {
 var person2 = {
     age: 27,
     firstName: 'Max',
+    hobbies: ['Cooking', 'Sports'],
+    greet: function (lastName) {
+        console.log('Hi, I am ' + this.firstName + ' ' + lastName);
+    },
 };
-greet({ firstName: 'Foo', age: 16 });
+// greet({ firstName: 'Foo', age: 16 });
 changeName(person2, 'Anna');
 greet(person2);
+person2.greet('Lootz');
+var Person2 = /** @class */ (function () {
+    function Person2(firstName, age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+    Person2.prototype.greet = function (lastName) {
+        console.log('Hi, I am ' + this.firstName + ' ' + lastName);
+    };
+    return Person2;
+}());
+var myPerson = new Person2('John', 36);
+myPerson.greet('Doe');
+var myDoubleFunction;
+myDoubleFunction = function (num1, num2) {
+    return num1 + num2;
+};
+function double(n) {
+    return n * 2;
+}
+// Interface inheritance
 //# sourceMappingURL=interfaces.js.map
--- ts/interfaces.ts
+++ ts/interfaces.ts
@@ -2,6 +2,7 @@ interface INamedPerson {
   firstName: string;
   age?: number;
   [propName: string]: any;
+  greet(lastName: string): void;
 }
 
 function greet(person: INamedPerson) {
@@ -15,8 +16,50 @@ function changeName(person: INamedPerson, newName: string) {
 const person2 = {
   age: 27,
   firstName: 'Max',
+  hobbies: ['Cooking', 'Sports'],
+
+  greet(lastName: string) {
+    console.log('Hi, I am ' + this.firstName + ' ' + lastName);
+  },
 };
 
-greet({ firstName: 'Foo', age: 16 });
+// greet({ firstName: 'Foo', age: 16 });
 changeName(person2, 'Anna');
 greet(person2);
+person2.greet('Lootz');
+
+class Person2 implements INamedPerson {
+  public firstName: string;
+  public age: number;
+
+  constructor(firstName: string, age: number) {
+    this.firstName = firstName;
+    this.age = age;
+  }
+
+  public greet(lastName: string): void {
+    console.log('Hi, I am ' + this.firstName + ' ' + lastName);
+  }
+}
+
+const myPerson = new Person2('John', 36);
+
+myPerson.greet('Doe');
+
+
+// Function Types
+interface IDoubleValueFunc {
+  (number1: number, number2: number): number;
+}
+
+let myDoubleFunction: IDoubleValueFunc;
+myDoubleFunction = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+function double(n: number) {
+  return n * 2;
+}
+
+
+// Interface inheritance
